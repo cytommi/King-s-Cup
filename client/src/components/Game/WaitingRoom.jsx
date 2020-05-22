eb3bda54049f0dec32c3ff20a0cb989ea7f512f4
Thomas Wang <cytommi@gmail.com>
2020-05-22 15:09:23 -0500

    fixed spinner

--- client/src/components/Game/WaitingRoom.jsx
+++ client/src/components/Game/WaitingRoom.jsx
@@ -5,7 +5,7 @@ import '../../styling/waitingRoom.scss';
 const WaitingRoom = () => (
   <div id="waiting-room">
     <h1>Waiting for next round to begin... Take this time to hydrate.</h1>
-    <Spinner name="pacman" color="#71bbff" fadeIn="1" className="spinner" />
+    <Spinner name="pacman" color="#71bbff" className="spinner" />
   </div>
 );
 export default WaitingRoom;
