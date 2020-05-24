0df79e7bc5fed8d480192a27d97400aeeb31790d
Thomas Wang <cytommi@gmail.com>
2020-05-24 16:11:30 -0500

    webpack prod

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -81,11 +81,11 @@ module.exports = {
         description:
           'Play the famous drinking game online for free with your friends! No registration required!',
         robots: 'index, nofollow',
-        'og:title': `Play King's Cup!`,
-        'og:description':
-          'Play the famous drinking game online with friends for free. No registration required!',
-        'og:url': 'https://www.cytommigames.com',
-        'og:type': 'website',
+        // 'og:title': `Play King's Cup!`,
+        // 'og:description':
+        //   'Play the famous drinking game online with friends for free. No registration required!',
+        // 'og:url': 'https://www.cytommigames.com',
+        // 'og:type': 'website',
       },
       favicon: path.join(__dirname, './src/assets/icons/favicon.ico'),
     }),
