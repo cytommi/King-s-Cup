ec294143b4ec70f0810148c969865ace9e4cbd2d
Thomas Wang <cytommi@gmail.com>
2020-05-24 15:48:21 -0500

    webpack

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -86,7 +86,7 @@ module.exports = {
           'Play the famous drinking game online with friends for free. No registration required!',
         'og:url': 'https://www.cytommigames.com',
         'og:type': 'website',
-        'og:image': path.join(__dirname, './src/assets/images/Kings_Cup.png'),
+        // 'og:image': path.join(__dirname, './src/assets/images/Kings_Cup.png'),
       },
       favicon: path.join(__dirname, './src/assets/icons/favicon.ico'),
       cache: false,
